1520563b757f6ec76a91f151d0d57423c7e779ed
J. Matthew Myers <myers3jm@mail.uc.edu>
2024-01-04 00:05:20 -0500

    Adding zip download

--- main/main.py
+++ main/main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pandas as pd
 import Event
 from Event import Event
 import Letter
 from Letter import Letter
+import pdfkit
+import zipfile
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -35,13 +38,28 @@ def upload():
 
         event = Event(eventTitle.title(), location, timeframe)
         
-        letters = []
+        pdfLetters = []
 
         for entry in df.values:
             letter = Letter(event, location, entry[0], entry[1], entry[2], entry[3], 'sincerely', 'J. Matthew Myers')
-            letters.append(letter)
+            try:
+                pdfLetter = pdfkit.from_string(str(letter))
+                pdfLetters.append(pdfLetter)
+            except Exception as e:
+                return f'Failure during conversion to PDF: {e.with_traceback()}'
+            
+        def create_zip_archive(pdf_data_list):
+            zip_buffer = BytesIO()
+            with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+                for index, pdf_data in enumerate(pdf_data_list):
+                    # Add each PDF to the zip archive with a unique name
+                    pdf_filename = f'document_{index + 1}.pdf'
+                    zip_file.writestr(pdf_filename, pdf_data)
+            zip_buffer.seek(0)
+            return zip_buffer
+        zip_file = create_zip_archive(pdfLetters)
+        return send_file(zip_file, download_name='letters.zip', as_attachment=True, mimetype='application/zip')
 
-        return f'{letters[0]}'
     else:
         return "Invalid file format. Please upload a .xlsx file"
 
